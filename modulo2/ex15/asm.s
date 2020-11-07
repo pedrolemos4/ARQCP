c662f51770940149cb749cba0314854ece7430ef
Tiago_1191098 <1191098@isep.ipp.pt>
2020-11-07 16:33:29 +0000

    Módulo2, Exercício 15

--- modulo2/ex15/asm.s
+++ modulo2/ex15/asm.s
@@ -1,5 +1,10 @@
-.section .text
+#.section .data
+#.global A
+#.global B
+#.global C
+#.global D
 
+.section .text
 .global compute
 compute:
 
@@ -9,16 +14,25 @@ movl %esp, %ebp 	# the stack frame pointer for sum function
 
 #body of the function
 movl A, %eax
+cdq
 movl B, %ecx
-imul %ecx
+imull %ecx
 
 movl C, %ecx
 addl %ecx,%eax
-cdq
+cmpl $0, D
+je D_is_0
+
 movl D, %ecx
 idivl %ecx
+jmp end
 
+D_is_0:
+	movl $0, %eax
+	jmp end
+	
+end:	
 # epilogue
-	movl %ebp, %esp
-    popl %ebp
-    ret
+movl %ebp, %esp
+popl %ebp
+ret
--- modulo2/ex15/main.c
+++ modulo2/ex15/main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include "asm.h"
 
-int A;
-int B;
-int C;
+int A=0;
+int B=0;
+int C=0;
 int D;
 
 int main()
 {
-	int res;
+	int res=0;
+	printf("Introduza o valor de A:\n");
+	scanf("%d", &A);
+	printf("Introduza o valor de B:\n");
+	scanf("%d", &B);
+	printf("Introduza o valor de C:\n");
+	scanf("%d", &C);
+	printf("Introduza o valor de D:\n");
+	scanf("%d", &D);
 	res=compute();
+	printf("Resultado: %d\n",res);
 	return 0;	
 }	
