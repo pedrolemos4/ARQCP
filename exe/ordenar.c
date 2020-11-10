fabc2212da2aca02d1b0e06fb0a9d82e803fa340
Pedro <1190974@isep.ipp.pt>
2020-11-10 15:11:39 +0000

    comments

--- exe/ordenar.c
+++ exe/ordenar.c
@@ -4,16 +4,16 @@ void ordenar(char *str){
 	
 	int i, k, cont=0, aux;
 	
-	while(* ( str + cont ) != '\0'){
-		cont++;						//a variavel cont fica com o tamanho de str
+	while(* ( str + cont ) != '\0'){ // percorre a string até a posição estiver vazia
+		cont++;						//a variável cont fica com o tamanho de str
 	}
 
-	for(i=0; i<cont; i++){
-		for(k=i+1; k<cont; k++){
-			if(*(str + k) < *(str + i) ){
-				aux=*(str + i);
-				*(str + i) = *(str + k);
-				*(str + k) = aux;
+	for(i=0; i<cont; i++){					//percorre carater a carater da string
+		for(k=i+1; k<cont; k++){			//percorre carater a carater da string mas vai 1 posicao adiantada ao for anterior
+			if(*(str + k) < *(str + i) ){	//compara os valores de cada carater de acordo com a tabela ascii
+				aux=*(str + i);				//atribui o valor da tabela ascii do carater a aux para ordenar
+				*(str + i) = *(str + k);	//trocam de posições ficando ordenados
+				*(str + k) = aux;			//atribui o valor de *(str+i) a *(str+k)
 			}
 		}
 	}	
