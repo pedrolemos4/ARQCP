82116bec52bde093edc3527d0aec7b0f09dfa897
Tiago_1191098 <1191098@isep.ipp.pt>
2020-11-16 18:58:05 +0000

    Módulo3, Exercício 9, adição de comentários

--- modulo3/ex09/asm.s
+++ modulo3/ex09/asm.s
@@ -5,7 +5,6 @@ vec_search:
 	#prologue
 	pushl %ebp				 	#save previous stack frame pointer
 	movl %esp, %ebp			 	#the stack frame pointe for our function
-	#pushl %esi
 	
 	#body of the function
 	movl num, %ecx			 	#cópia do valor de num para o registo %cx
@@ -13,27 +12,25 @@ vec_search:
 	je impossible			 	#jump se %cx é 0
 	
 	movl ptrvec,%edx	 		#cópia do endereço de ptrvec para o registo %esi
-	#movswl x,%edx
-	movw x, %ax				 	#cópia de 0 para %eax
+	movw x, %ax				 	#cópia do valor de x para %ax
 	
 	vec_loop:
-		cmpw %ax,(%edx)
-		je found_x
-		addl $2,%edx
-		loop vec_loop
-		movl $0,%eax
-		jmp end
+		cmpw %ax,(%edx)			#verificação se o short int apontado pelo endereço em %edx é igual ao valor de %ax
+		je found_x				#jump se são iguais
+		addl $2,%edx			#avança para o short int seguinte de prtvec (apontado pelo endereço em %edx)
+		loop vec_loop			#avança para a iteração seguinte do loop, se %ecx for maior que 0
+		movl $0,%eax			#cópia de 0 para %eax
+		jmp end					#salta para o fim da função
 	
 	found_x:
-		movl %edx, %eax
-		jmp end
+		movl %edx, %eax			#cópia do endereço em que %edx se encontra para %eax
+		jmp end					#salta para o fim da função
 		
 	impossible:
-		movl $0, %eax
+		movl $0, %eax			#cópia do valor 0 para %eax
 		
 	end:
 		#epilogue
-		#popl %esi
 		movl %ebp, %esp			 #restore the stack pointer
 		popl %ebp				 #restore the stack frame pointer
 		ret						 #return from the function
