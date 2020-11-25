06488ac4e171f58e6dd4730dce8091bdaa8d25a4
Tiago_1191098 <1191098@isep.ipp.pt>
2020-11-25 14:12:05 +0000

    Módulo 4 ,Exercício 3, adição de comentários

--- modulo4/ex03/asm.s
+++ modulo4/ex03/asm.s
@@ -5,24 +5,24 @@ greatest:
 	pushl %ebp				 			#save previous stack frame pointer
 	movl %esp, %ebp			 			#the stack frame pointe for our function
 	
-	movl 16(%ebp),%edx
-	movl 12(%ebp),%ecx
-	movl 8(%ebp),%eax
+	movl 16(%ebp),%edx					#move para o registo edx o valor do terceiro parâmetro
+	movl 12(%ebp),%ecx					#move para o registo ecx o valor do segundo parâmetro
+	movl 8(%ebp),%eax				    #move para o registo eax o valor do primeiro parâmetro
 	
-	cmp %eax,%ecx
-	jg ecx_is_greater
-	cmp %eax,%edx
-	jg edx_is_greater
-	jmp end
+	cmp %eax,%ecx						#verifica se o valor de ecx é maior que o valor de eax
+	jg ecx_is_greater					#jump se ecx é maior
+	cmp %eax,%edx						#verifica se o valor de edx é maior que o valor de eax
+	jg edx_is_greater					#jump se edx é maior
+	jmp end								#avança para o fim
 	
 	ecx_is_greater:
-		cmp %ecx,%edx
-		jg edx_is_greater
-		movl %ecx,%eax
-		jmp end
+		cmp %ecx,%edx					#verifica se o valor de edx é maior que ecx
+		jg edx_is_greater				#jump se edx for maior
+		movl %ecx,%eax					#move para o registo eax (registo de retorno) o valor de ecx (maior valor)
+		jmp end							#avança para o fim
 	
 	edx_is_greater:
-		movl %edx,%eax
+		movl %edx,%eax					#move para o registo eax (registo de retorno) o valor de edx (maior valor)
 		
 	end:
 	#epilogue
